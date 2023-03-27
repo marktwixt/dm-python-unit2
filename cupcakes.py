7713230192c28b5ab3d573650b98e2eea64505f6
Walt Ostrander <waltostrander@gmail.com>
2023-03-26 19:35:05 -0500

    completed part 3, fixed code to store the sprinkles as a pipe-separated string in the CSV file.

--- cupcakes.py
+++ cupcakes.py
@@ -1,3 +1,5 @@
+import csv
+from pprint import pprint
 from abc import ABC, abstractmethod
 
 class Cupcake(ABC):
@@ -19,14 +21,16 @@ class Cupcake(ABC):
     def calculate_price(self, quantity):
         pass
 
+
 class Mini(Cupcake):
     size = "mini"
 
-    def __init__(self, name, price, flavor, frosting):
-        super().__init__(name, price, flavor, frosting, None)
+    def __init__(self, name, price, flavor, frosting, filling):
+        super().__init__(name, price, flavor, frosting, filling)
 
     def calculate_price(self, quantity):
         return quantity * self.price
+
 
 class Large(Cupcake):
     size = "large"
@@ -35,27 +39,68 @@ class Large(Cupcake):
         super().__init__(name, price, flavor, frosting, filling)
 
     def calculate_price(self, quantity):
-        return quantity * self.price * 1.5
+        return quantity * self.price
 
-class Vegan(Cupcake):
-    def __init__(self, name, price, flavor, frosting, filling):
-        super().__init__(name, price, flavor, frosting, filling)
+def read_csv(file):
+    with open(file, newline="\n") as csvfile:
+        reader = csv.DictReader(csvfile)
 
-    def calculate_price(self, quantity):
-        vegan_cost = self.price * 1.2
-        return quantity * vegan_cost
+        for row in reader:
+            row["sprinkles"] = row["sprinkles"].split("|")
+            pprint(row)
 
-class GlutenFree(Cupcake):
-    def __init__(self, name, price, flavor, frosting, filling):
-        super().__init__(name, price, flavor, frosting, filling)
+def write_new_csv(file, cupcakes):
+    with open(file, "w", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-    def calculate_price(self, quantity):
-        gluten_free_cost = self.price * 1.3
-        return quantity * gluten_free_cost
+        for cupcake in cupcakes:
+            cupcake_dict = {
+                "size": cupcake.size,
+                "name": cupcake.name,
+                "price": cupcake.price,
+                "flavor": cupcake.flavor,
+                "frosting": cupcake.frosting,
+                "sprinkles": "|".join(cupcake.sprinkles),
+                "filling": cupcake.filling,
+            }
+            writer.writerow(cupcake_dict)
+
+def append_cupcake(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        cupcake_dict = {
+            "size": cupcake.size,
+            "name": cupcake.name,
+            "price": cupcake.price,
+            "flavor": cupcake.flavor,
+            "frosting": cupcake.frosting,
+            "sprinkles": "|".join(cupcake.sprinkles),
+            "filling": cupcake.filling,
+        }
+        writer.writerow(cupcake_dict)
 
 if __name__ == "__main__":
-    vegan_cupcake = Vegan("Vegan Chocolate Delight", 3.00, "chocolate", "vanilla", "ganache")
-    gluten_free_cupcake = GlutenFree("Gluten-Free Chocolate Delight", 3.00, "chocolate", "vanilla", "ganache")
+    # Create a list of cupcake instances
+    cupcakes = [
+        Mini("Mini Chocolate Dream", 2.50, "chocolate", "chocolate", None),
+        Mini("Mini Vanilla Delight", 2.50, "vanilla", "vanilla", None),
+        Mini("Mini Strawberry", 2.50, "strawberry", "strawberry", None),
+        Large("Large Chocolate Dream", 4.50, "chocolate", "chocolate", None),
+        Large("Large Vanilla Delight", 4.50, "vanilla", "vanilla", None),
+        Large("Large Strawberry", 4.50, "strawberry", "strawberry", None)
+    ]
 
-    print(f"Vegan cupcake price for 4 cupcakes: ${vegan_cupcake.calculate_price(4):.2f}")
-    print(f"Gluten-free cupcake price for 4 cupcakes: ${gluten_free_cupcake.calculate_price(4):.2f}")
+    # Write the new CSV file with the list of cupcakes
+write_new_csv("cupcakes.csv", cupcakes)
+
+# Append a new cupcake to the existing CSV file
+new_cupcake = Large("Large Red Velvet", 4.50,
+                    "red velvet", "cream cheese", None)
+append_cupcake("cupcakes.csv", new_cupcake)
+
+# Read the CSV file to verify the cupcakes have been added
+read_csv("cupcakes.csv")
